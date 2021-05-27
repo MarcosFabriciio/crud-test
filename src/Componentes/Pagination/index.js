35190db7f90940a00c5742ca752378668a192f7d
Marcos <mfos@ic.ufal.br>
2021-05-27 17:02:03 -0300

    Create pagnations componet

--- src/Componentes/Pagination/index.js
+++ src/Componentes/Pagination/index.js
@@ -1,29 +1,35 @@
 import { Pagination } from "react-bootstrap"
 
 export default function Pag(data) {
-   let active = data.page.current;
+   let currentPage = data.page.current;
    let totalPages = data.page.total;
-
-   let items = [];
-   for (let number = 1; number <= 5; number++) {
-      items.push(
-         <Pagination.Item key={number} active={number === active}>
-            {number}
-         </Pagination.Item>,
-      );
-   }
 
    return (
       <div>
          <Pagination>
             <Pagination.First />
             <Pagination.Prev />
-            <Pagination.Ellipsis />
-
-            <Pagination.Item active>{active}</Pagination.Item>
-
-            <Pagination.Ellipsis />
-            <Pagination.Item>{20}</Pagination.Item>
+            { 
+               currentPage >= 3 ? (
+                  <Pagination.Ellipsis />
+               ) : null 
+            } 
+            {
+               currentPage >= 2 ? (
+                  <Pagination.Item >{currentPage - 1}</Pagination.Item>                  
+               ) : null
+            }
+            <Pagination.Item active>{currentPage}</Pagination.Item>
+            {
+               totalPages - currentPage >= 1 ? (
+                  <Pagination.Item >{currentPage + 1}</Pagination.Item>                  
+               ) : null
+            }
+            { 
+               totalPages - currentPage >= 2 ? (
+                  <Pagination.Ellipsis />
+               ) : null 
+            }
             <Pagination.Next />
             <Pagination.Last />
          </Pagination>
